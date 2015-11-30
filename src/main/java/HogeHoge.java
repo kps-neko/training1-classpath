58b4f72a3d5f0c020a129adc96c12ca9a0169383
s-wada <s-wada@keyportsolutions.com>
2015-11-30 11:53:42 +0900

    練習問題のアップ

---
+++ src/main/java/HogeHoge.java
@@ -0,0 +1,10 @@
+import hogehoge.fugafuga.PiyoPiyo;
+
+/**
+ * Created by s-wada on 2015/11/26.
+ */
+public class HogeHoge {
+    public static void main (String[] args) {
+        PiyoPiyo.piyo();
+    }
+}
---
+++ src/main/java/hogehoge/fugafuga/PiyoPiyo.java
@@ -0,0 +1,12 @@
+package hogehoge.fugafuga;
+
+import javax.net.ssl.SSLContext;
+
+/**
+ * Created by s-wada on 2015/11/26.
+ */
+public class PiyoPiyo {
+    public static void piyo() {
+        System.out.println("piyopiyo");
+    }
+}
